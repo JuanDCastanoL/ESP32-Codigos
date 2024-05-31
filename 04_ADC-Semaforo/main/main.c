6a40a871d0598d41d31392f109fec87cb2547c32
JuanDCastanoL <mrdrop65@gmail.com>
2024-05-31 16:24:31 -0500

    Codigo ADC funcional

--- 04_ADC-Semaforo/main/main.c
+++ 04_ADC-Semaforo/main/main.c
@@ -8,10 +8,15 @@
 #include "esp_adc/adc_cali_scheme.h"
 
 static const char *TAG = "EXAMPLE";
-//#define canal_pot ADC1_CHANNEL_4        // GPIO32
 
-static int adc_raw[2][10];
-static int voltage[2][10];
+#define CANAL_ADC ADC_CHANNEL_4        // GPIO32 - ADC1 CHANNEL4
+#define ATTEN_ADC ADC_ATTEN_DB_12
+#define BITWH_ADC ADC_BITWIDTH_12
+
+ 
+
+static int adc_raw[2];
+static int voltage[2];
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
@@ -26,22 +31,22 @@ void app_main(void)
 
     //-------------ADC1 Config---------------//
     adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BITWIDTH_12,
-        .atten = ADC_ATTEN_DB_12,
+        .bitwidth = BITWH_ADC,
+        .atten = ATTEN_ADC,
     };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_4, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, CANAL_ADC, &config));
 
     //-------------ADC1 Calibration Init---------------//
     adc_cali_handle_t adc1_cali_chan4_handle = NULL;
-    bool do_calibration1_chan4 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_4, ADC_ATTEN_DB_12, &adc1_cali_chan4_handle);
+    bool do_calibration1_chan4 = example_adc_calibration_init(ADC_UNIT_1, CANAL_ADC, ATTEN_ADC, &adc1_cali_chan4_handle);
 
     while(1){
         
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_4, &adc_raw[0][0]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_4, adc_raw[0][0]);
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, CANAL_ADC, &adc_raw[0]));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, CANAL_ADC, adc_raw[0]);
         if (do_calibration1_chan4) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan4_handle, adc_raw[0][0], &voltage[0][0]));
-            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_CHANNEL_4, voltage[0][0]);
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan4_handle, adc_raw[0], &voltage[0]));
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, CANAL_ADC, voltage[0]);
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
