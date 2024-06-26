1c03d80d916022760f1a9505e112321574a3435c
JuanDCastanoL <mrdrop65@gmail.com>
2024-06-25 19:30:26 -0500

    Se agregó el código correspondiente al LDR

--- 04_ADC-Semaforo/main/main.c
+++ 04_ADC-Semaforo/main/main.c
@@ -16,15 +16,15 @@
 
 SemaphoreHandle_t xSemaphore       = NULL;
 TaskHandle_t   xHandle_task_pot    = NULL;
-//TaskHandle_t   xHandle_task_ntc    = NULL;
+TaskHandle_t   xHandle_task_ldr    = NULL;
 adc_oneshot_unit_handle_t adc1_handle;
 led_rgb_t led_rgb_1;
-led_rgb_t led_rgb_2;
+
 
 #define ATTEN_ADC ADC_ATTEN_DB_12
 #define BITWH_ADC ADC_BITWIDTH_12
-#define CANAL_ADC_POT ADC_CHANNEL_4            // GPIO32 - ADC1 CHANNEL4 - Potenciometro
-//#define CANAL_ADC_NTC ADC_CHANNEL_4
+//#define CANAL_ADC_POT ADC_CHANNEL_4            // GPIO32 - ADC1 CHANNEL4 - Potenciometro
+#define CANAL_ADC_LDR ADC_CHANNEL_6            // GPIO34 - ADC1 CHANNEL6 - Resistencia LDR 
 #define SAMPLES               (20) 
 
 static const char *TAG = "EXAMPLE";
@@ -36,13 +36,12 @@ static int adc_raw[2];
 #define GPIO_LED_1_R           (13)
 #define GPIO_LED_1_G           (12)
 #define GPIO_LED_1_B           (14)
-#define GPIO_LED_2_R           (27)
-#define GPIO_LED_2_G           (26)
-#define GPIO_LED_2_B           (25)
 
 
+/*
 void task_pot(void *arg)
 {
+    int cont = 0;
     for(;;){
 
         if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE){
@@ -59,20 +58,55 @@ void task_pot(void *arg)
                 RGB_CHANGE(led_rgb_1, 255, 0, 0);    
             }
        
-            xSemaphoreGive(xSemaphore);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+            if (cont == 10) {
+                cont = 0;
+                xSemaphoreGive(xSemaphore);
+            } else {
+                cont++;
+                vTaskDelay(500 / portTICK_PERIOD_MS); 
+            }
+                            
 
         }
     } 
         
 }
-
-/*
-void task_ntc(void *arg)
-{
-
-}
 */
+
+
+void task_ldr(void *arg)
+{
+    
+    for (;;)
+    {
+        if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE){
+
+                        
+            adc_oneshot_read_multisample(adc1_handle, CANAL_ADC_LDR, &adc_raw[1], SAMPLES);
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, CANAL_ADC_LDR, adc_raw[1]);
+
+            if (adc_raw[1] < 900){
+                RGB_CHANGE(led_rgb_1, 170, 0, 25);
+            } else if (adc_raw[1] < 1800){
+                RGB_CHANGE(led_rgb_1, 70, 30, 25);
+            } else if (adc_raw[1] < 2600){
+                RGB_CHANGE(led_rgb_1, 30, 70, 25);
+            } else {
+                RGB_CHANGE(led_rgb_1, 0, 170, 25);
+            }
+
+
+            xSemaphoreGive(xSemaphore);
+            
+            vTaskDelay(500 / portTICK_PERIOD_MS); 
+            
+            
+        }
+    }
+    
+}
+
 
 
 
@@ -91,17 +125,15 @@ void app_main(void)
     //---------------- ↑ Semaforo Set-Up ↑ -------------------//
 
 
-
     //---------------- ↓ Tareas Set-Up ↓------------------//
 
-    xTaskCreatePinnedToCore(task_pot, "T_POT", 8192, NULL, 0, &xHandle_task_pot, 1);
-    configASSERT( xHandle_task_pot );
+    //xTaskCreatePinnedToCore(task_pot, "T_POT", 8192, NULL, 0, &xHandle_task_pot, 1);
+    //configASSERT( xHandle_task_pot );
 
-    //xTaskCreatePinnedToCore(task_ntc, "T_NTC", 2048, NULL, 0, &xHandle_task_ntc, 1);
-    //configASSERT( xHandle_task_ntc );
+    xTaskCreatePinnedToCore(task_ldr, "T_LDR", 2048, NULL, 0, &xHandle_task_ldr, 1);
+    configASSERT( xHandle_task_ldr );
 
     //---------------- ↑ Tareas Set-Up ↑ -------------------//
-
 
 
     //---------------- ↓ ADC Set-Up ↓------------------//
@@ -115,13 +147,8 @@ void app_main(void)
         .bitwidth = BITWH_ADC,
         .atten = ATTEN_ADC,
     };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, CANAL_ADC_POT, &config));   // Conf canal 4 - Potenciometro
-    //ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, CANAL_ADC_POT, &config));   // Conf canal 4 - 
-
-    /*    Si se requiere de la curva de calibración
-    adc_cali_handle_t adc1_cali_chan4_handle = NULL;
-    bool do_calibration1_chan4 = example_adc_calibration_init(ADC_UNIT_1, CANAL_ADC_POT, ATTEN_ADC, &adc1_cali_chan4_handle);   // Calib ADC1-Canal4
-    */
+    //ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, CANAL_ADC_POT, &config));   // Conf canal 4 - Potenciometro
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, CANAL_ADC_LDR, &config));   // Conf canal 6 - Resistencia LDR
 
     //---------------- ↑ ADC Set-Up ↑ -------------------//
 
@@ -130,7 +157,6 @@ void app_main(void)
 
     RGB_TIMER_INIT();
     led_rgb_1 = RGB_CHANNEL_INIT_1(GPIO_LED_1_R, GPIO_LED_1_G, GPIO_LED_1_B);
-    //led_rgb_2 = RGB_CHANNEL_INIT_2(GPIO_LED_2_R, GPIO_LED_2_G, GPIO_LED_2_B);
 
     //---------------- ↑ RGB Set-Up ↑ -------------------//
 
@@ -142,10 +168,13 @@ void app_main(void)
 
 
 
+/*    Si se requiere de la curva de calibración, en el set up
+adc_cali_handle_t adc1_cali_chan4_handle = NULL;
+bool do_calibration1_chan4 = example_adc_calibration_init(ADC_UNIT_1, CANAL_ADC_POT, ATTEN_ADC, &adc1_cali_chan4_handle);   // Calib ADC1-Canal4
+*/
 
 
-
-/*    Si se desea pasar la lectura a voltaje teniendo en cuenta la curva de calibración
+/*    Si se desea pasar la lectura a voltaje teniendo en cuenta la curva de calibración, donde se requiera
 if (do_calibration1_chan4) {
     ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan4_handle, adc_raw[0], &voltage[0]));
     ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, CANAL_ADC_POT, voltage[0]);
